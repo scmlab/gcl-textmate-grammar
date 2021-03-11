c02e1eca68ce9931ccb4d42dd58c479e39b1ddcd
Ting-Gian LUA <banacorn@gmail.com>
2021-03-11 15:27:47 +0800

    [ new ] Serialize the grammar as JSON

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -1,39 +1,126 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Grammar where
 
-import Data.Map
+import Data.Aeson
+import Data.Aeson.Types (Pair)
+import Data.Map ( foldrWithKey, Map )
+import Data.Text ( pack, Text )
 
 -- | Top level structure
 data Grammar = Grammar
-  { scopeName :: String,
-    fileTypes :: [String],
-    foldingStartMarker :: Maybe String,
-    foldingStopMarker :: Maybe String,
-    patterns :: [String],
-    firstLineMatch :: Maybe String,
-    repository :: Repository
+  { grammarScopeName :: Text,
+    grammarFileTypes :: [Text],
+    grammarFoldingStartMarker :: Maybe Text,
+    grammarFoldingStopMarker :: Maybe Text,
+    grammarFirstLineMatch :: Maybe Text,
+    grammarPatterns :: [Reference],
+    grammarRepository :: Repository
   }
 
 -- | Repository
-type Repository = Map String Rule
+type Repository = Map Text Rule
 
 -- | Rule
 data Rule = Rule
-  { ruleName :: String,
+  { 
+    -- | "name"
+    ruleName :: Maybe Text,
+    -- | "contentName"
+    ruleContentName :: Maybe Text,
+    -- | "match" and "captures"
     ruleMatch :: Maybe Capture,
+    -- | "begin" and "beginCaptures"
     ruleBegin :: Maybe Capture,
+    -- | "end" and "endCaptures"
     ruleEnd :: Maybe Capture,
-    ruleContentName :: Maybe String,
-    ruleInclude :: Maybe Reference
+    -- | "include"
+    ruleInclude :: [Reference]
   }
 
+include :: Reference -> Rule
+include ref =
+  Rule
+    { ruleName = Nothing,
+      ruleMatch = Nothing,
+      ruleBegin = Nothing,
+      ruleEnd = Nothing,
+      ruleContentName = Nothing,
+      ruleInclude = [ref]
+    }
+
 -- | A capturing group
-data Capture = Capture String (Map Int String)
+data Capture = Capture String (Map Int Text)
 
 -- | Reference in "include"
 data Reference
   = -- | Generates "$self"
     Self
   | -- | e.g. Place "variable" to generate "#variable"
-    Ref String
+    Ref Text
   | -- | e.g. Place "hs" to generate "source.hs"
-    Lang String
+    Lang Text
+
+--------------------------------------------------------------------------------
+
+instance ToJSON Grammar where
+  toJSON grammar =
+    object $
+      [ "scopeName" .= grammarScopeName grammar,
+        "fileTypes" .= grammarFileTypes grammar,
+        -- "patterns" .= grammarPatterns grammar,
+        "repository" .= grammarRepository grammar
+      ]
+        -- <> ["patterns" .= grammarPatterns grammar]
+        -- <> ["patterns" .= map (\ref -> ref) (grammarPatterns grammar)]
+        <> ["patterns" .= map (\ref -> object ["include" .= ref]) (grammarPatterns grammar)]
+        <> "foldingStartMarker" .? grammarFoldingStartMarker grammar
+        <> "foldingStopMarker" .? grammarFoldingStopMarker grammar
+        <> "firstLineMatch" .? grammarFirstLineMatch grammar
+
+instance ToJSON Rule where
+  toJSON rule =
+    object $
+      match
+        <> begin
+        <> end
+        <> "name" .? ruleName rule
+        <> "contentName" .? ruleContentName rule
+        <> include 
+        
+    where
+      match = case ruleMatch rule of
+        Nothing -> []
+        Just (Capture regex groups) ->
+          [ "match" .= regex,
+            "captures" .= foldCaptureGroups groups
+          ]
+
+      begin = case ruleBegin rule of
+        Nothing -> []
+        Just (Capture regex groups) ->
+          [ "begin" .= regex,
+            "beginCaptures" .= foldCaptureGroups groups
+          ]
+
+      end = case ruleEnd rule of
+        Nothing -> []
+        Just (Capture regex groups) ->
+          [ "end" .= regex,
+            "endCaptures" .= foldCaptureGroups groups
+          ]
+
+      include = ["patterns" .= map (\ref -> object ["include" .= ref]) (ruleInclude rule)]
+
+infixr 8 .?
+
+(.?) :: (KeyValue a, ToJSON v) => Text -> Maybe v -> [a]
+key .? field = maybe [] (\x -> [key .= x]) field
+
+foldCaptureGroups :: Map Int Text -> Value
+foldCaptureGroups = object . foldrWithKey (\key value acc -> acc <> [pack (show key) .= object ["name" .= value]]) []
+
+instance ToJSON Reference where
+  toJSON Self = "$self"
+  toJSON (Ref s) = String $ "#" <> s
+  toJSON (Lang s) = String $ "source." <> s
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,81 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Aeson (encode)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map as Map
+import Grammar
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = BS.writeFile "output.tmLanguage.json" (encode grammar)
+
+grammar :: Grammar
+grammar =
+  Grammar
+    { grammarScopeName = "source.gcl",
+      grammarFileTypes = ["gcl"],
+      grammarFoldingStartMarker = Nothing,
+      grammarFoldingStopMarker = Nothing,
+      grammarFirstLineMatch = Nothing,
+      grammarPatterns = references,
+      grammarRepository = repository
+    }
+
+references :: [Reference]
+references =
+  [ Ref "skip",
+    Ref "abort"
+  ]
+
+repository :: Repository
+repository =
+  Map.fromList
+    [ ("skip", skip),
+      ("abort", abort)
+    ]
+  where 
+    skip :: Rule
+    skip =
+      Rule
+        { ruleBegin = Nothing,
+          ruleEnd = Nothing,
+          ruleMatch = Just (Capture "skip" Map.empty),
+          ruleName = Just "keyword.control.skip",
+          ruleInclude = [],
+          ruleContentName = Nothing
+        }
+
+    abort :: Rule
+    abort =
+      Rule
+        { ruleBegin = Nothing,
+          ruleEnd = Nothing,
+          ruleMatch = Just (Capture "abort" Map.empty),
+          ruleName = Just "keyword.control.abort",
+          ruleInclude = [],
+          ruleContentName = Nothing
+        }
+
+
+-- declaration :: Rule
+-- declaration =
+--   Rule
+--     { ruleBegin = Just $ Capture "^var" $ Map.fromList [(0, "storage.modifier")],
+--       ruleEnd = Just $ Capture "(?=^\\S)" Map.empty,
+--       ruleMatch = Nothing,
+--       ruleName = Just "meta.declaration",
+--       ruleInclude = [Ref "declaration-var"],
+--       ruleContentName = Nothing
+--     }
+
+declarationVar :: Rule
+declarationVar =
+  Rule
+    { ruleBegin = Just $ Capture "^var" $ Map.fromList [(0, "storage.modifier")],
+      ruleEnd = Just $ Capture "(?=^\\S)" Map.empty,
+      ruleMatch = Nothing,
+      ruleName = Just "meta.declaration",
+      ruleInclude = [Ref "built-in-type", Ref "declaration-var-names"],
+      ruleContentName = Nothing
+    }
